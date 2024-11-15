f3ed0724ed1ddaae499f0b8b42c0dd856089cdb2
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-11-15 20:46:09 +0330

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,50 +24,62 @@ export class AppComponent {
   //   this.vcr()?.createComponent(ChildComponent);
   // }
 
-  //2=> استفاده از viewChild و ایجاد یک container که کامپوننت داخل آن ساخته میشود
+  // مرحله2
+  // استفاده از viewChild و ایجاد یک container که کامپوننت داخل آن ساخته میشود
   vcr = viewChild.required('container', { read: ViewContainerRef });
-
+  // یا
   // @ViewChild('container', { read: ViewContainerRef }) vcr!: ViewContainerRef;
 
   private componentRef!: ComponentRef<ChildComponent>;
 
-  //7=> مرحله7
-  // private contentProjection = viewChild.required(ContentComponent, {
-  //   read: ElementRef,
-  // });
+  // مرحله7
+  private contentProjection = viewChild.required(ContentComponent, {
+    read: ElementRef,
+  });
 
   //مرحله8
-  private contentTpl = viewChild.required('content', { read: TemplateRef });
+  private contentTpl = viewChild.required('contentTpl', { read: TemplateRef });
 
   createChildComponent() {
+    // مرحله2
     // this.vcr()?.clear();
     // this.componentRef = this.vcr().createComponent(ChildComponent);
+    // یا
     // this.vcr?.createComponent(ChildComponent);
-    //7=> content projection => با استفاده از ElementRef
-    // this.componentRef = this.vcr().createComponent(ChildComponent, {
-    //   projectableNodes: [[this.contentProjection().nativeElement]],
-    // });
-    //8=> content projection => با استفاده از TemplateRef
-    const contentTpl = this.vcr().createEmbeddedView(this.contentTpl());
+
+    // مرحله7
+    // content projection => با استفاده از ElementRef
     this.componentRef = this.vcr().createComponent(ChildComponent, {
-      projectableNodes: [contentTpl.rootNodes],
+      projectableNodes: [[this.contentProjection().nativeElement]],
     });
+
+    // مرحله8
+    // content projection => با استفاده از TemplateRef
+    // const contentTpl = this.vcr().createEmbeddedView(this.contentTpl());
+    // this.componentRef = this.vcr().createComponent(ChildComponent, {
+    //   projectableNodes: [contentTpl.rootNodes],
+    // });
 
     // 5=>اگر بخواهیم input برای کامپوننت set کنیم
     this.componentRef!.setInput('name', 'mamadoo');
 
-    //6=> برای دسترسی به output
+    // مرحله6
+    // برای دسترسی به output
     // this.componentRef.instance.clear.subscribe(() => {
     //   this.deleteChildComponent();
     // });
   }
 
   deleteChildComponent() {
-    // تمام viewContainer ها را پاک میکند<=3
+    // مرحله3
+    // پاک کردن vcr
+    //تمام viewContainer ها را پاک میکند
     // this.vcr()?.clear();
-    //  4=> با ایندکس viewContainer مورد نظر را پاک میکند
+    // مرحله4
+    //با ایندکس viewContainer مورد نظر را پاک میکند
     // this.vcr()?.remove(0);
-    // 5=>آن component را حذف میکند که آخرین instance را در componentRef ذخیره کرده ایم
+    // مرحله5
+    // آن component را حذف میکند که آخرین instance را در componentRef ذخیره کرده ایم
     // this.componentRef.destroy();
   }
 }
