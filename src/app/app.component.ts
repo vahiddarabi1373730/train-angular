65a074590a9eac1ce93190f2df31962ab31500d0
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-11-24 09:56:24 +0330

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
 import { ButtonComponent, SelectComponent } from 'my-lib';
+//مرحله1
+//npm install verdaccio -g
+
+//مرحله2
+//verdaccio
+//با زدن این کد اطلاعاتی را برمیگرداند که یکی از آن ها
+//http://localhost:4873
+
+// مرحله3
+// وارد این آدرس شده
+// http://localhost:4873
+
+// مرحله4
+// npm adduser --registry http://localhost:4873
+
+//مرحله5
+// npm whoami --registry http://localhost:4873
+// باید نام کاربری را برگرداند
+
+// مرحله6
+// library را build گرفته
+// وارد dist->my-lib
+// در این directory
+// npm publish --registry http://localhost:4873
 
 @Component({
   selector: 'app-root',
