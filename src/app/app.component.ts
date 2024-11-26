ac417a35d02b727350f402646b5f6865a146b7ad
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-11-26 14:46:48 +0330

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  // مرحله2
+  // استفاده از host در Component@
+  host: {
+    '[class.hovered]': 'isHover',
+    '(mouseenter)': 'onMouseEnter()',
+    '(mouseleave)': 'onMouseLeave()',
+  },
 })
 export class AppComponent {
+  // مرحله 1
+  // @HostBinding('class.hovered') isHover = false;
+  //
+  // @HostListener('mouseenter') onMouseEnter() {
+  //   this.isHover = true;
+  // }
+  //
+  // @HostListener('mouseleave') onMouseLeave() {
+  //   this.isHover = false;
+  // }
+
+  // مرحله2
+  // استفاده از host در Component@
+  isHover = false;
+
+  onMouseEnter() {
+    this.isHover = true;
+  }
+
+  onMouseLeave() {
+    this.isHover = false;
+  }
 }
