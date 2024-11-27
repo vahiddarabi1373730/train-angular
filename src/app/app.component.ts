9f70316a3b169f622418cae0c78dc25267a68ef6
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-11-27 17:54:31 +0330

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface UserInterface {
+  id: number;
+  name: string;
+  type: UserType;
+}
+
+export type UserType = 'user' | 'admin' | 'other';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +16,10 @@ import {Component} from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  protected isLoadUser = true;
+  protected user: UserInterface[] = [
+    { id: 1, name: 'vahid', type: 'admin' },
+    { id: 2, name: 'hamed', type: 'user' },
+    { id: 3, name: 'ali', type: 'other' },
+  ];
 }
