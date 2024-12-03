4b2b109ccaed8078d592ee6559af5f71b07cb0ca
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-12-03 15:31:31 +0330

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import {Component} from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
+
+export interface TodoInterface {
+  userId: number;
+  id: number;
+  title: number;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    VirtualScrollerModule,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  protected numbers = signal<any[]>(
+    Array.from({ length: 10000 }).map((value, i) => i),
+  );
 }
