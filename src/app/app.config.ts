b6068b5991f1b8817a8e35588a8c14afc627d0b6
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-12-04 21:28:29 +0330

    commit2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+//provideExperimentalZonelessChangeDetection
+// با این کد zone را از کار انداخته
+// ولی initialChangeDetection انگولار که نمیتوان آن را از کار انداخت 1 بار و فقط 1 بار اجرا میشود
+// در نتیجه اگر Log ها را ببینی 10 تا لاگ داریم
+// ولی اگر این کد را نزاریم ما 13 تا لاگ داریم
+// 10 تا برای initialChangeDetection که همیشه است
+// 3 تا برای run شدن مجددا changeDetection
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideExperimentalZonelessChangeDetection(),
+  ],
 };
