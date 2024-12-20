6f1baa0d0f9aee59110e066f8765b32ff07d17df
vahiddarabi1373730 <db.vahid1737@gmail.com>
2024-12-20 13:53:42 +0330

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  DynamicFormInterface,
+  FormGeneratorComponent,
+} from 'vahid-share-components';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  public config: DynamicFormInterface = {
+    controls: [
+      {
+        name: 'name',
+        type: 'string',
+        controlType: 'input',
+        validators: { required: true },
+        label: 'Name',
+        value: null,
+      },
+      {
+        name: 'family',
+        type: 'string',
+        controlType: 'input',
+        validators: { required: true },
+        label: 'Family',
+        value: null,
+      },
+    ],
+  };
+
+  onReadyForm(form: FormGroup) {
+    form.controls['name'].valueChanges.subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
